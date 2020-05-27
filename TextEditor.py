0cbad0664d482c2bc75fbd2971e313c459dc4de0
Igor-Felipy <igor.felipy123@gmail.com>
2020-05-27 18:26:45 -0400

    comentado

--- TextEditor.py
+++ TextEditor.py
@@ -1,6 +1,6 @@
+#fazer a verificação da versão do python no sistema
 import sys
 v = sys.version
-
 if "2.7" in v:
     from Tkinter import *
     import tkFileDialog
@@ -8,10 +8,13 @@ elif "3." in v:
     from tkinter import *
     from tkinter import filedialog
 
+#definição da tela
 root = Tk()
 text = Text(root)
 text.grid()
 
+
+#metodo de salvamento
 def saveas():
     global text
     t = text.get("1.0","end-1c")
@@ -22,25 +25,29 @@ def saveas():
 button = Button(root, text="Save", command=saveas)
 button.grid()
 
+#trocar font para helvetica
 def FontHelvetica():
     global text
     text.config(font="Helvetica")
 
 
+#trocar font para courier
 def FontCourier():
     global text
     tex.config(font=Courier)
 
 
+#meu de troca de fonte
 font = Menubutton(root, text="Font")
 font.grid()
 font.menu = Menu(font, tearoff=0)
 font["menu"] = font.menu
 helvetica = IntVar()
 courier = IntVar()
-
 font.menu.add_checkbutton(label="Courier", variable=courier, command=FontCourier)
 font.menu.add_checkbutton(label="Helvetica", variable=helvetica, command=FontHelvetica)
 
 
+
+#rodando o programa
 root.mainloop()
